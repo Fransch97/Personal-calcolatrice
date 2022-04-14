47042e2e88772d352a58280c942c89b0b3dae21a
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-04-14 21:34:02 +0200

    finished first

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ let checker = false;
 //calling the buttons and understand if you want firstValue ore secondValue
 button[0].addEventListener('click', function(){ 
     if(!checker){
+        document.querySelector('h1').innerHTML = " ";
         document.querySelector('h1').innerHTML = button[0].firstChild.nodeValue
         checker = true
     }else{
@@ -39,6 +40,7 @@ button[0].addEventListener('click', function(){
 
 button[1].addEventListener('click', function(){ 
     if(!checker){
+        document.querySelector('h1').innerHTML = "";
         document.querySelector('h1').innerHTML = button[1].firstChild.nodeValue
         checker = true
     }else{
@@ -49,6 +51,7 @@ button[1].addEventListener('click', function(){
 
 button[2].addEventListener('click', function(){ 
     if(!checker){
+        document.querySelector('h1').innerHTML = "";
         document.querySelector('h1').innerHTML = button[2].firstChild.nodeValue
         checker = true
     }else{
@@ -59,6 +62,8 @@ button[2].addEventListener('click', function(){
 
 button[3].addEventListener('click', function(){ 
     if(!checker){
+        console.log("i am")
+        document.querySelector('h1').innerHTML = "";
         document.querySelector('h1').innerHTML = button[3].firstChild.nodeValue
         checker = true
     }else{
@@ -69,6 +74,8 @@ button[3].addEventListener('click', function(){
 
 button[4].addEventListener('click', function(){ 
     if(!checker){
+        console.log("i am")
+        document.querySelector('h1').innerHTML = "";
         document.querySelector('h1').innerHTML = button[4].firstChild.nodeValue
         checker = true
     }else{
@@ -79,6 +86,8 @@ button[4].addEventListener('click', function(){
 
 button[5].addEventListener('click', function(){ 
     if(!checker){
+        console.log("i am")
+        document.querySelector('h1').innerHTML = "";
         document.querySelector('h1').innerHTML = button[5].firstChild.nodeValue
         checker = true
     }else{
@@ -89,6 +98,7 @@ button[5].addEventListener('click', function(){
 
 button[6].addEventListener('click', function(){ 
     if(!checker){
+        console.log("i am")
         document.querySelector('h1').innerHTML = button[6].firstChild.nodeValue
         checker = true
     }else{
@@ -99,6 +109,7 @@ button[6].addEventListener('click', function(){
 
 button[7].addEventListener('click', function(){ 
     if(!checker){
+        console.log("i am")
         document.querySelector('h1').innerHTML = button[7].firstChild.nodeValue
         checker = true
     }else{
@@ -109,6 +120,7 @@ button[7].addEventListener('click', function(){
 
 button[8].addEventListener('click', function(){ 
     if(!checker){
+        console.log("i am")
         document.querySelector('h1').innerHTML = button[8].firstChild.nodeValue
         checker = true
     }else{
@@ -119,6 +131,7 @@ button[8].addEventListener('click', function(){
 
 button[9].addEventListener('click', function(){ 
     if(!checker){
+        console.log("i am")
         document.querySelector('h1').innerHTML = button[9].firstChild.nodeValue
         checker = true
     }else{
@@ -126,6 +139,18 @@ button[9].addEventListener('click', function(){
 
     }
 })
+button[10].addEventListener('click', function(){ 
+        console.log("i am")
+        if(!checker){
+        document.querySelector('h1').innerHTML = button[10].firstChild.nodeValue
+        checker = true
+    }else{
+        document.querySelector('h1').innerHTML += button[10].firstChild.nodeValue
+
+    }
+})
+
+
 
 const operators = document.querySelectorAll('.op');
 console.log(operators);
@@ -135,14 +160,15 @@ operators[0].addEventListener('click', function(){
     operatorChoise = "addition";
     checker=false;
     firstValue = document.querySelector('h1').firstChild.nodeValue;
-    console.log(firstValue)
-console.log(operatorChoise)
+    console.log("this ist first value",firstValue)
+    console.log("this is operatorChoise",operatorChoise)
 })
 
 operators[1].addEventListener('click', function(){
     console.log(operators[1]);
     operatorChoise = "subtraction";
     checker=false;
+    firstValue = document.querySelector('h1').firstChild.nodeValue;
     console.log(operatorChoise)
 })
 
@@ -150,6 +176,7 @@ operators[2].addEventListener('click', function(){
     console.log(operators[2]);
     operatorChoise = "divisor";
     checker=false;
+    firstValue = document.querySelector('h1').firstChild.nodeValue;
     console.log(operatorChoise)
 })
 
@@ -157,6 +184,7 @@ operators[3].addEventListener('click', function(){
     console.log(operators[3]);
     operatorChoise = "multiplicator";
     console.log(operatorChoise)
+    firstValue = document.querySelector('h1').firstChild.nodeValue;
     checker=false;
 })
 
@@ -168,9 +196,27 @@ const results = []
 
 startCalc.addEventListener('click', function(){
     secondValue = document.querySelector('h1').firstChild.nodeValue;
+    console.log("this is secondValue", secondValue)
     if(firstValue && secondValue ){
         if(operatorChoise === "addition"){
-            
+            document.querySelector('h1').innerHTML = parseFloat(firstValue) + parseFloat(secondValue);
+            firstValue = "";
+            secondValue = "";
+        }
+         if(operatorChoise === "subtraction"){
+            document.querySelector('h1').innerHTML = parseFloat(firstValue) - parseFloat(secondValue);
+            firstValue = "";
+            secondValue = "";
+        }
+         if(operatorChoise === "divisor"){
+            document.querySelector('h1').innerHTML = parseFloat(firstValue) / parseFloat(secondValue);
+            firstValue = "";
+            secondValue = "";
+        }
+        if(operatorChoise === "multiplicator"){
+            document.querySelector('h1').innerHTML = parseFloat(firstValue) * parseFloat(secondValue);
+            firstValue = "";
+            secondValue = "";
         }
     }
 })
